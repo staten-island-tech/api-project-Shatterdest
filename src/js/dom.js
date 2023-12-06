18a1dc1a14d6c653ce1404f94ef2026adba31e15
Shatterdest <73318855+Shatterdest@users.noreply.github.com>
2023-12-06 13:13:34 -0500

    HOW DO I ADD AN EVENT LISTNER

--- src/js/dom.js
+++ src/js/dom.js
@@ -2,6 +2,7 @@ export function domSelectors() {
     return {
         search: document.getElementById('search'),
         searchInput: document.getElementById('searchInput'),
-        lContainer: document.getElementById('location-container')
+        lContainer: document.getElementById('location-container'),
+        locations: document.querySelectorAll("#location")
     }
 }
--- src/js/main.js
+++ src/js/main.js
@@ -5,10 +5,18 @@ const key = `D4b4dAzj6RLOGGm46BucLTLpzz2Z9Kzi`;
 
 dom.searchInput.value = null;
 
-function search(key, query) {
-  const url = `http://dataservice.accuweather.com/locations/v1/search?apikey=${key}&q=${query}`;
-  console.log(url);
-  return url;
+function search(key, query, option) {
+  if (option === "search") {
+    const url = `http://dataservice.accuweather.com/locations/v1/search?apikey=${key}&q=${query}`;
+    console.log(url);
+    return url;
+  } else if (option === "weather") {
+    const url = `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${query}?apikey=${key}`;
+    console.log(url);
+    return url;
+  } else {
+    console.log("how");
+  }
 }
 
 async function getData(URL) {
@@ -31,17 +39,26 @@ dom.search.addEventListener("submit", async (e) => {
   e.preventDefault();
   const submitted = dom.searchInput.value;
   console.log(`value submitted: ${submitted}`);
-  const locations = await getData(search(key, submitted));
+  const locations = await getData(search(key, submitted, "search"));
   console.log(locations);
   locations.forEach((location) => {
     dom.lContainer.insertAdjacentHTML(
       "beforeend",
-      `<div class="location">
-    <p>${location.EnglishName}</p>
-    <p>${location.Region.EnglishName}</p>
-    <p>${location.Country.EnglishName}</p>
+      `<div class="location" value='${location.Key}'>
+    <h3 class='region-name'>${location.EnglishName}</h3>
+    <h4>${location.Region.EnglishName}</h4>
+    <h4>${location.Country.EnglishName}</h4>
     <p>${location.Type}</p>
   </div>`
     );
+  });
+  dom.locations.forEach((location) => {
+    console.log(`adding event lisetner for ${location}`);
+    location.addEventListener("click", async (e) => {
+      e.preventDefault();
+      const locationID = location.value;
+      const lData = await getData(search(key, locationID, "weather"));
+      console.log(lData);
+    });
   });
 });
